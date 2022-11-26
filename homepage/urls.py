a2b9ddc182e574ddf41d79d7498d3a6db91d421e
ramyanareswari <ramya.nareswari@ui.ac.id>
2022-11-26 23:12:18 +0700

    fix: change urls.py for flutter auth

--- homepage/urls.py
+++ homepage/urls.py
@@ -3,6 +3,10 @@ from homepage.views import show_homepage
 from homepage.views import register
 from homepage.views import login_user
 from homepage.views import logout_user
+# ========= flutter =========
+from homepage.views import login_flutter
+from homepage.views import register_flutter
+from homepage.views import logout_flutter
 app_name = 'homepage'
 
 urlpatterns = [
@@ -10,4 +14,7 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('loginflutter', login_flutter, name='login_flutter'),
+    path('registerflutter',register_flutter,name='register_flutter'),
+    path('logoutflutter',logout_flutter,name='logout_flutter'),
 ]
