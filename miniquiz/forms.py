5371365648487d91d1cfae647551cc82b36a4873
Shamira Alifanindya Prasetya <shamira.alifanindya@ui.ac.id>
2022-12-11 23:04:18 +0700

    fix miniquiz module again

--- miniquiz/forms.py
+++ miniquiz/forms.py
@@ -2,7 +2,6 @@ from django.forms import ModelForm
 from miniquiz.models import *
 
 # (DONE) Create form.
-# jadi redundant forms-nya cokkkk
 class MiniQuizForm(ModelForm):
     class Meta :
         model = QuizModel
