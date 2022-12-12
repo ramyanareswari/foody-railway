c55455f6b4ea40343c0b9186a466d80391a5d991
Nikolas Reva Bellie <nikolas.reva@ui.ac.id>
2022-12-12 18:20:52 +0700

    Dari bener ko jadi errorr

--- send_foodwaste/views.py
+++ send_foodwaste/views.py
@@ -22,7 +22,6 @@ def show_foodwaste_json(request):
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")  
 
 @login_required(login_url='/login/')
-@csrf_exempt
 def add_foodwaste(request):
     if request.method == "POST":
         user = request.user
