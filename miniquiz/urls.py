f5d5fab33472b29af99824d7b0a0c80af40e7552
Shamira Alifanindya Prasetya <shamira.alifanindya@ui.ac.id>
2022-12-12 19:46:06 +0700

    nyoba benerin lagi

--- miniquiz/urls.py
+++ miniquiz/urls.py
@@ -12,8 +12,8 @@ urlpatterns = [
     
     path('get-quiz-model/', get_quiz_model, name='get-quiz-model'),
     path('get-quiz-beneran/<pk>', get_quiz_beneran, name='get-quiz-beneran'),
-    path('get-quiz-model/<pk>', get_question, name='get-question'),
-    path('get-quiz-model/<pk>/<pk2>', get_option, name='get-option'),
-    path('get-quiz-model/<pk>/save', save_assessment, name='save-assessment'),
+    #path('get-quiz-model/<pk>', get_question, name='get-question'),
+    #path('get-quiz-model/<pk>/<pk2>', get_option, name='get-option'),
+    #path('get-quiz-model/<pk>/save', save_assessment, name='save-assessment'),
 
 ]
