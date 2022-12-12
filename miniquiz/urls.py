8cf1e996c0f62ceb59f7afdc2231fbe393c6a925
Shamira Alifanindya Prasetya <shamira.alifanindya@ui.ac.id>
2022-12-12 19:56:17 +0700

    nyoba benerin lagi dan lagi

--- miniquiz/urls.py
+++ miniquiz/urls.py
@@ -6,14 +6,13 @@ app_name = 'miniquiz'
 urlpatterns = [
     path('', show_quiz_homepage, name='show-quiz-homepage'),
     
+    path('get-quiz-model/', get_quiz_model, name='get-quiz-model'),
+    path('get-quiz-beneran/<pk>', get_quiz_beneran, name='get-quiz-beneran'),
+    path('get-quiz-model/<pk>', get_question, name='get-question'),
+    path('get-quiz-model/<pk>/<pk2>', get_option, name='get-option'),
+    path('get-quiz-model/<pk>/save', save_assessment, name='save-assessment'),
+
     path('<pk>/', show_quiz_mainpage, name='show-quiz-mainpage'),
     path('<pk>/data/', quiz_data_view, name='quiz-data-view'),
     path('<pk>/save', save_quiz, name='save-quiz'),
-    
-    path('get-quiz-model/', get_quiz_model, name='get-quiz-model'),
-    path('get-quiz-beneran/<pk>', get_quiz_beneran, name='get-quiz-beneran'),
-    #path('get-quiz-model/<pk>', get_question, name='get-question'),
-    #path('get-quiz-model/<pk>/<pk2>', get_option, name='get-option'),
-    #path('get-quiz-model/<pk>/save', save_assessment, name='save-assessment'),
-
 ]
